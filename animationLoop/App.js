66198e6a1c00c1a297b613868575836ef882f8dc
Matheus <mthluiz99@gmail.com>
2024-01-17 16:23:48 -0300

    adding loop animation

---
+++ animationLoop/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Animated } from "react-native";
+
+export default function App() {
+  // Inicializa um valor animado para a largura e altura
+  const largAnimated = new Animated.Value(150);
+
+  useEffect(() => {
+    // Efeito para a animação da largura
+
+    Animated.loop(Animated.sequence([])).start();
+  }, [largAnimated]);
+
+  return (
+    <View style={styles.container}>
+      {/* Componente animado com base nos valores animados */}
+      <Animated.View
+        style={{
+          width: largAnimated,
+          height: 300,
+          backgroundColor: "#000",
+        }}
+      ></Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
